10381fbf2cea809131b64dda452cc6425e056e5a
Dmytro <solonar96@gmail.com>
2021-11-24 17:51:43 +0200

    Transformation of cycles to functions, functions start, showMyDB, rememberMyFilms, detectPersonalLevel & writeYourGenres

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,28 +20,69 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    // if (a === null || a === false || a.length > 50 || b === null || b === false || b.length > 50) {
-    //     a = prompt('Один из последних просмотренных фильмов?', ''),
-    //     b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        // if (a === null || a === false || a.length > 50 || b === null || b === false || b.length > 50) {
+        //     a = prompt('Один из последних просмотренных фильмов?', ''),
+        //     b = prompt('На сколько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-console.log(personalMovieDB);
-if (+personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (+personalMovieDB.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (+personalMovieDB.count > 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Ошибка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (+personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (+personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (+personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Ошибка');
+    }
 }
+
+detectPersonalLevel();
+
+// function showMyDB() {
+//     if (personalMovieDB.privat) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+console.log(personalMovieDB);
+
+// Срез строки работает и без последнего символа:
+// если таким образом указать, то обрезано будет с данного символа и до конца.
+
+// Если в срезе указать отрицательные значения, то счёт индексов смволов будет начат не с начала, а с конца.
+
+// Для среза строки можно использовать substring (не работает с отрицательными значениями)
+// и substr (вместо последнего символа указывается необходимая длинна среза).
+
